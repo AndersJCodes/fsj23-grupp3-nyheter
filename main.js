1bd04f7fda9279b77681bffd48558c2b14c25dac
Chalel <charlie03.pl@gmail.com>
2024-01-24 17:07:19 +0100

    Gick tillbaka till gamla designen

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import './style.css'
 import axios from "axios";
 
-const key ="d75aac48decd4e9c8d02e380452a20ea";
+const key = "d75aac48decd4e9c8d02e380452a20ea";
 const url = `https://newsapi.org/v2/everything?q=javascript&html&react&CSS&typescript&apiKey=${key}`;
 const articleElement = document.querySelector("#article")
 
@@ -19,21 +19,22 @@ getUser(url);
 
 function displayArticles(articles) {
   const html = articles.map(article => `
-    <div class="card" style="width: 40rem">
-      <div class="card-header">Category</div>
-        <img src="${article.urlToImage}" class="card-img-top" alt="..." />
-        <div class="card-body">
-          <h5 class="card-title">${article.title}</h5>
-          <p class="card-text">${article.description}</p>
+    <div class="articleCard">
+      <div class="cardHeader">Category
+        <div class="Marked"><img src="star.png" style="width:20px; height:20px"></div>
+      </div>
+       ${article.urlToImage ? `<img src="${article.urlToImage}" class="card-img-top" alt="..." />` : ''}
+        <div class="cardBody">
+          <h5 class="cardTitle">${article.title}</h5>
+          <p class="cardText">${article.description}</p>
           <a href="${article.url}" class="btn btn-primary">Read the full article</a>
-          <span>Source:${article.name}</span>
+        <span>Source:${article.name}</span>
         </div>
       </div>
     </div>
   `)
 
   articleElement.innerHTML = html;
-
 }
 
 
