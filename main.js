28b6d362bbd9122a43ac2f8ce96fd30ac23b7426
julia.paulin <julia.paulin@chasacademy.se>
2024-01-25 14:31:29 +0100

    upd fetch functionality

--- main.js
+++ main.js
@@ -1,27 +1,34 @@
-import { key } from './configApi';
-import './style.css';
+import { key } from "./configApi";
+import "./style.css";
 import axios from "axios";
 
-const url = `https://newsapi.org/v2/everything?q=javascript&html&react&CSS&typescript&apiKey=${key.API_KEY_2}`;
-const articleElement = document.querySelector("#article")
+//const reactWord = "react.js";
+const searchDefault = "javascript&css";
+const articleElement = document.querySelector("#article");
 
-async function getUser(url) {
+async function getNews(searchWord) {
   try {
+    const url = `https://newsapi.org/v2/everything?language=en&q=${searchWord}&apiKey=${key.API_KEY_2}`;
     const response = await axios.get(url);
-    //console.log(response.data.articles);
+    console.log(response.data.articles);
     // console.log(url);
-    displayArticles(response.data.articles)
+    displayArticles(response.data.articles);
   } catch (error) {
     console.error(error);
   }
 }
-getUser(url);
+getNews(searchDefault);
 
 function displayArticles(articles) {
-  const html = articles.map(article => `
+  const html = articles.map(
+    (article) => `
     <div class="articleCard">
       
-       ${article.urlToImage ? `<img src="${article.urlToImage}" class="card-img-top" alt="..." />` : ''}
+       ${
+         article.urlToImage
+           ? `<img src="${article.urlToImage}" class="card-img-top" alt="..." />`
+           : ""
+       }
        <div class="cardHeader">Category
         <div class="Marked"><img src="star.png" style="width:20px; height:20px"></div>
       </div>
@@ -29,16 +36,18 @@ function displayArticles(articles) {
         <div class="cardBody">
           <h5 class="cardTitle">${article.title}</h5>
           <p class="cardText">${article.description}</p>
-          <a href="${article.url}" class="btn btn-primary">Read the full article</a>
+          <a href="${
+            article.url
+          }" class="btn btn-primary">Read the full article</a>
         <span>Source:${article.name}</span>
         </div>
       </div>
     </div>
-  `)
+  `
+  );
 
   articleElement.innerHTML = html;
 }
-
 
 /* 
 const display = (response) => {
