25cfba66c4d29b61a57c66c88b7369b4f0b234b8
Chalel <charlie03.pl@gmail.com>
2024-01-26 10:57:28 +0100

    Artikel visar skapar författare om författare inte finns visas source som unkown. Artiklar utan beskrivning visas inte längre.

--- main.js
+++ main.js
@@ -20,7 +20,9 @@ async function getNews(searchWord) {
 getNews(searchDefault);
 
 function displayArticles(articles) {
-  const html = articles.map(article => `
+  const html = articles
+    .filter(article => article.description)
+    .map(article => `
     <div class="articleCard"> 
       <div class="cardHeader">Category
         <div class="Marked"><img src="star.png" style="width:20px; height:20px"></div>
@@ -29,15 +31,14 @@ function displayArticles(articles) {
         <div class="cardBody">
           <h5 class="cardTitle">${article.title}</h5>
           <p class="cardText">${article.description}</p>
-          <a href="${
-            article.url
-          }" class="btn btn-primary">Read the full article</a>
-        <span>Source:${article.name}</span>
+          <a href="${article.url
+      }" class="btn btn-primary">Read the full article</a>
+        <span>Source: ${article.author !== null ? article.author : 'Unkown'}</span>
         </div>
       </div>
     </div>
   `
-  );
+    );
 
   articleElement.innerHTML = html;
 }
