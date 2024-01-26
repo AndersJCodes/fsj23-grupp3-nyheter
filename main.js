ee82c35352f6758e683ffa64035ac62ddb606c5d
julia.paulin <julia.paulin@chasacademy.se>
2024-01-26 21:08:55 +0100

    started on search on word

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 import { key } from "./configApi";
 import "./style.css";
 import axios from "axios";
+/* import {
+  searchWordButton,
+  searchInputField, searchOnWord,
+} from "./searchword.js"; */
 
 //const reactWord = "react.js";
 const searchDefault = "javascript&css";
@@ -8,9 +12,9 @@ const articleElement = document.querySelector("#article");
 
 async function getNews(searchWord) {
   try {
-    const url = `https://newsapi.org/v2/everything?language=en&q=${searchWord}&apiKey=${key.API_KEY_2}`;
+    const url = `https://newsapi.org/v2/everything?language=en&searchIn=title&q=${searchWord}&apiKey=${key.API_KEY_2}`;
     const response = await axios.get(url);
-    console.log(response.data.articles);
+    // console.log(response.data.articles);
     // console.log(url);
     displayArticles(response.data.articles);
   } catch (error) {
@@ -20,12 +24,17 @@ async function getNews(searchWord) {
 getNews(searchDefault);
 
 function displayArticles(articles) {
-  const html = articles.map(article => `
+  const html = articles.map(
+    (article) => `
     <div class="articleCard"> 
       <div class="cardHeader">Category
         <div class="Marked"><img src="star.png" style="width:20px; height:20px"></div>
       </div>
-       ${article.urlToImage ? `<img src="${article.urlToImage}" class="card-img-top" alt="..." />` : ''}
+       ${
+         article.urlToImage
+           ? `<img src="${article.urlToImage}" class="card-img-top" alt="..." />`
+           : ""
+       }
         <div class="cardBody">
           <h5 class="cardTitle">${article.title}</h5>
           <p class="cardText">${article.description}</p>
@@ -42,8 +51,19 @@ function displayArticles(articles) {
   articleElement.innerHTML = html;
 }
 
-/* 
-const display = (response) => {
-const titleName = respone.articles[0].title
-} 
-getNews(exampleurl).then(display);*/
+//Search on word
+const searchWordButton = document.querySelector("#search-word-button");
+let searchInputField = document.querySelector("#search-input");
+
+const searchOnWord = (searchInputField) => {
+  let word = searchInputField.value;
+  getNews(word);
+};
+
+searchWordButton.addEventListener("click", (e) => {
+  searchOnWord(searchInputField);
+
+  if (e) {
+    searchInputField.value = "";
+  }
+});
