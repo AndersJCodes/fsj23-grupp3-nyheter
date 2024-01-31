df5ad164ae40039031328ea784e4ede429915c92
AndoGPP <146361135+AndoGPP@users.noreply.github.com>
2024-01-31 13:24:22 +0100

    not sure what this update is

--- favorites.js
+++ favorites.js
@@ -19,24 +19,4 @@ export function listenForFavorites() {
     }
   })
 };
-/* 
-export function listenForFavorites() {
-  // Initialize a variable to track the favorite state
-  let isFavorite = false;
 
-  // Add a click event listener to the document
-  document.addEventListener("click", function (event) {
-    // Check if the clicked element has the class "favorite"
-    if (event.target.classList.contains("favorite")) {
-      // Toggle the favorite state variable
-      isFavorite = !isFavorite;
-
-      // Update the image based on the current state
-      const clickedFavoriteButton = event.target;
-      clickedFavoriteButton.src = isFavorite ? './favorite-true.png' : './favorite-false.png';
-
-      // Log the current state for debugging
-      console.log("Favorite button clicked. Current state:", isFavorite);
-    }
-  })
-}; */
