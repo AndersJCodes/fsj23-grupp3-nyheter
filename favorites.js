682c6ee84a554b54a959b69654fa4ee742422506
AndoGPP <146361135+AndoGPP@users.noreply.github.com>
2024-01-31 09:50:35 +0100

    updated favorites

--- favorites.js
+++ favorites.js
@@ -1,4 +1,4 @@
-'use strict'
+
 
 //Listen to the generated buttons
 
@@ -23,28 +23,5 @@ export function listenForFavorites() {
       // Log the current state for debugging
       console.log("Favorite button clicked. Current state:", isFavorite);
     }
-  });
-}
-
-/* export function listenForFavorites() {
-  // Initialize a variable to track the favorite state
-  let isFavorite = false;
-
-  // Select all current and future favorite buttons
-  const favoritesBtn = document.querySelectorAll(".favorite");
-
-  // Add a click event listener to each favorite button
-  favoritesBtn.forEach(button => {
-    button.addEventListener("click", function (event) {
-      // Toggle the favorite state variable
-      isFavorite = !isFavorite;
-
-      // Update the image based on the current state
-      const clickedFavoriteButton = event.target;
-      clickedFavoriteButton.src = isFavorite ? './favorite-true.png' : './favorite-false.png';
-
-      // Log the current state for debugging
-      console.log("Favorite button clicked. Current state:", isFavorite);
-    });
-  });
-} */
+  })
+};
