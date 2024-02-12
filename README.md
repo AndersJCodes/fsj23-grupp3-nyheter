bbf1e45fbfe73422c7bcfffc38235616ce6c56ab
julia.paulin <julia.paulin@chasacademy.se>
2024-02-12 14:55:22 +0100

    sign in welcome, readme

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# nyheter
+# Nyheter
+```
+https://github.com/AndoGPP/fsj23-grupp3-nyheter
+```
+
+## Description
+This project is a news site about Javascript, CSS, HTML, and React. The news is retrieved with Axios from an open news API called News API. 
+The user can sort the news by date, get the latest articles within a category, mark the news as a favorite, and save it in his account. The user can also find news by searching for a keyword. All functions that need to be saved use Firebase. The site is published at Github Pages and tracked and analyzed in Google Analytics. 
+
+```
+API  
+News API - https://newsapi.org/v2/everything?language=en&q=${searchDefault}&excludeDomains=${excludeDomains}&apiKey=${your_api_key_here}
+```
+
+```  
+Jira
+https://andogpp.atlassian.net/wiki/home
+https://andogpp.atlassian.net/jira/projects?selectedProjectType=software
+```
+
+## About 
+The project was carried out by Jakob Mehamed, William Janssen, Charlie Ålander, Julia Paulin and Anders Jacobson.
--- auth.js
+++ auth.js
@@ -1,54 +1,50 @@
-
 // Import the functions you need from the SDKs you need
-import {firebaseConfig} from "./configApi";
+import { firebaseConfig } from "./configApi";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import {
-getAuth,
-createUserWithEmailAndPassword,
-signInWithEmailAndPassword,
-onAuthStateChanged,
-signOut
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
 } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-auth.js";
-import validation,{removeValidation} from "./validation";
+import validation, { removeValidation } from "./validation";
 
+const displayWelome = document.querySelector(".welcome");
 
-function registerNewUser(){
-    const joinBtn = document.querySelector("#registerBtn");
-    
-    joinBtn.addEventListener("click",(e)=>{
-        e.preventDefault();
-        const joinEmail= document.querySelector("#joinEmail");
-        const joinPassword = document.querySelector("#joinPassword");
+function registerNewUser() {
+  const joinBtn = document.querySelector("#registerBtn");
 
-        registerUserToFirebase(joinEmail,joinPassword)
+  joinBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const joinEmail = document.querySelector("#joinEmail");
+    const joinPassword = document.querySelector("#joinPassword");
 
-    }); 
-        
+    registerUserToFirebase(joinEmail, joinPassword);
+  });
 }
 
-function signInUser(){
-    const signInBtn = document.querySelector("#signInBtn");
-    
-    signInBtn.addEventListener("click",(e)=>{
-        e.preventDefault();
-        const signInEmail = document.querySelector("#signInEmail");
-        const signInPassword = document.querySelector("#signInPassword");
+function signInUser() {
+  const signInBtn = document.querySelector("#signInBtn");
 
-        authenticatUserInFirebase(signInEmail,signInPassword);
-    }); 
-        
+  signInBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const signInEmail = document.querySelector("#signInEmail");
+    const signInPassword = document.querySelector("#signInPassword");
+
+    authenticatUserInFirebase(signInEmail, signInPassword);
+  });
 }
 
-function signOutUser(){
-    const signOutBtn = document.querySelector("#signOutBtn");
-    
-    signOutBtn.addEventListener("click",(e)=>{
-        e.preventDefault();
-        logoutUser();
-    }); 
-        
+function signOutUser() {
+  const signOutBtn = document.querySelector("#signOutBtn");
+
+  signOutBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    logoutUser();
+  });
 }
 
 // auth content
@@ -60,82 +56,69 @@ function signOutUser(){
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-const registerUserToFirebase = async(joinEmail,joinPassword) =>
-{
-    const userEmail = joinEmail.value;
-    const userPassword = joinPassword.value;
+const registerUserToFirebase = async (joinEmail, joinPassword) => {
+  const userEmail = joinEmail.value;
+  const userPassword = joinPassword.value;
 
-    createUserWithEmailAndPassword(auth,userEmail,userPassword)
-    .then(()=>{
-        alert("Your account has been created!")
+  createUserWithEmailAndPassword(auth, userEmail, userPassword)
+    .then(() => {
+      alert("Your account has been created!");
     })
     .catch((error) => {
-        validation('joinForm',error.message);
-        console.log(error.message);
-        
+      validation("joinForm", error.message);
+      console.log(error.message);
     });
-}
+};
 
-const authenticatUserInFirebase = async(signInEmail,signInPassword) =>
-{
-    const userEmail = signInEmail.value;
-    const userPassword = signInPassword.value;
-    signInWithEmailAndPassword(auth,userEmail,userPassword)
-    .then(()=>{
-        removeValidation(document.querySelector(`#signInForm`));
+const authenticatUserInFirebase = async (signInEmail, signInPassword) => {
+  const userEmail = signInEmail.value;
+  const userPassword = signInPassword.value;
+  signInWithEmailAndPassword(auth, userEmail, userPassword)
+    .then(() => {
+      removeValidation(document.querySelector(`#signInForm`));
     })
     .catch((error) => {
-
-        validation('signInForm',error.message);
-        console.log(errorCode + errorMessage);
+      validation("signInForm", error.message);
+      console.log(errorCode + errorMessage);
     });
+};
 
-}
-
-const logoutUser = ()=>
-{   
-    
-
-    signOut(auth)
-    .then(()=>
-    {
-        alert('You signed out')
-        
+const logoutUser = () => {
+  signOut(auth)
+    .then(() => {
+      alert("You signed out");
     })
-    .catch((err)=>{
-        console.log(err.message)
-    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
+
+onAuthStateChanged(auth, (user) => {
+  const signInBtn = document.querySelector("#signInModalBtn");
+  const joinBtn = document.querySelector("#joinModalBtn");
+  const signOutBtn = document.querySelector("#signOutBtn");
+  const savedNews = document.querySelector("#savedNews");
+  if (user === null) {
+    signInBtn.classList.remove("d-none");
+    joinBtn.classList.remove("d-none");
+    signOutBtn.classList.add("d-none");
+    savedNews.classList.add("d-none");
+    displayWelome.style.display = "none";
+  }
+  if (user !== null) {
+    savedNews.classList.remove("d-none");
+    signInBtn.classList.add("d-none");
+    joinBtn.classList.add("d-none");
+    signOutBtn.classList.remove("d-none");
+    displayWelome.style.display = "block";
+    displayWelome.innerHTML = "Welcome " + user.email
+  }
+});
+
+function authentication() {
+  registerNewUser();
+  signInUser();
+  signOutUser();
 }
 
-onAuthStateChanged(auth,(user)=>{
-    const signInBtn = document.querySelector('#signInModalBtn');
-    const joinBtn = document.querySelector('#joinModalBtn');
-    const signOutBtn = document.querySelector('#signOutBtn');
-    const savedNews = document.querySelector('#savedNews');
-    if(user===null)
-    {
-        
-        signInBtn.classList.remove("d-none");
-        joinBtn.classList.remove("d-none");
-        signOutBtn.classList.add("d-none");
-        savedNews.classList.add("d-none");
-        
-    }
-    if(user!==null)
-    {
-        savedNews.classList.remove("d-none");
-        signInBtn.classList.add("d-none");
-        joinBtn.classList.add("d-none");
-        signOutBtn.classList.remove("d-none");
-    }
-})
-
-function authentication(){
-    registerNewUser();
-    signInUser();
-    signOutUser();
-}
-
-
-
-export default authentication
+export default authentication;
