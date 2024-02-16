3cdbe3525bf4a7b804a7038af08a49fcbd9f51dd
AndoGPP <146361135+AndoGPP@users.noreply.github.com>
2024-02-16 10:20:03 +0100

    städade lite i koden favorites

--- favorites.js
+++ favorites.js
@@ -1,14 +1,16 @@
-//Listen to the generated buttons
-//debugger;
+////This module handles the favorites funktionality of the webpage that allows the user to star a news item. It is saved to local storage and then retrievved from local storage when the fetch is generated and also when the user choose to dislay the favorites only from the menu.
+
+//Declare variables needed for the function
 import { newsArr, displayArticles } from "./main";
+
 const savedNewsButton = document.querySelector("#save");
 const displaySavedNews = document.querySelector(".display-saved-news");
-
 export const favoriteItems = [];
 let favoriteUrl = "";
 
 favoriteItems.push(...populateFromFavorites()); //This lines populates favorites once the reload happens. favoriteItems is needed as it acts as a check against the handleFavoriteClick function below
 
+//Listen to a click on the favorites
 document.addEventListener("click", function (event) {
   if (event.target.classList.contains("favoriteBtn")) {
     handleFavoriteClick(event.target);
@@ -23,37 +25,39 @@ function handleFavoriteClick(target) {
 
   const favoriteUrl = target.dataset.url;
 
-  // Find the article corresponding to the clicked favorite button
+  //Finds the article corresponding to the clicked favorite button
   const updateFavorite = newsArr.find((item) => item.url === favoriteUrl);
 
-  // Check if the clicked article is already in favoriteItems
+  //Check if the clicked article is already in favoriteItems
   const indexInFavorites = favoriteItems.findIndex(
     (item) => item.url === favoriteUrl
   );
 
   if (indexInFavorites === -1) {
-    // Article is not in favorites, so add it
+    //Article is not in favorites, so add it
     favoriteItems.push(updateFavorite);
     console.log("Article added to favorites:", updateFavorite);
   } else {
-    // Article is already in favorites, so remove it
+    //Article is already in favorites, so remove it
     favoriteItems.splice(indexInFavorites, 1);
     console.log("Article removed from favorites:", updateFavorite);
   }
 
-  // Save the updated favorite list
+  //Saves the updated favorite list
   saveFavorite();
 
-  // Toggle the favorite button icon classes after saving the changes
+  //Toggle the favorite button icon class
   target.classList.toggle("fa-solid");
   target.classList.toggle("fa-regular");
   console.log("Favorite button toggled");
 }
 
+//Funktion that Updates the local storage with the changes made
 function saveFavorite() {
   localStorage.setItem("Favorites", JSON.stringify(favoriteItems));
 }
 
+//Function to load favorites from local storage, only used when loading the favorites button
 function loadFavorite() {
   const favoriteJSON = localStorage.getItem("Favorites");
   if (favoriteJSON == null) return [];
@@ -61,13 +65,11 @@ function loadFavorite() {
   displaySavedNews.style.display = "block";
 }
 
+//This function populates the fetch with the favorite items (somewhat duplicate with the above)
 export function populateFromFavorites() {
   const favoriteJSON = localStorage.getItem("Favorites");
   if (favoriteJSON == null) return [];
   return JSON.parse(favoriteJSON);
 }
-//add to favorites list
-
-//Listen to favorties button
 
 savedNewsButton.addEventListener("click", loadFavorite);
